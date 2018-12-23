81d2695f048ae4cce38c0fb686582f8582be43a1
Boni <boni619@gmail.com>
2018-12-24 00:37:42 +0530

    My First Node Js App

---
+++ app.js
@@ -0,0 +1,66 @@
+const express=require('express');
+const path=require('path');
+const bodyParser=require("body-parser");
+const nodemailer=require('nodemailer');
+
+let app=express();
+
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','jade');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',function(req,res){
+
+       res.render('index',{title:'Hi, There'});
+});
+app.get('/about',function(req,res){
+
+    res.render('about');
+});
+app.get('/contact',function(req,res){
+
+    res.render('contact');
+});
+app.post('/contact/send',function(req,res){
+
+    let transporter =  nodemailer.createTransport({
+          
+        service:'Gmail',
+        auth:{
+
+            user:'youremail@gmail.com',
+            pass:'yourpass'
+        }
+    });
+
+    let mailOptions = {
+
+        from: 'test(youremail@gmail.com)',
+        to:'anyemail@gmail.com',
+        subjet:'Contact user',
+        text: 'You have a submission with the following details... Name: '+req.body.name+'Email: '+req.body.email+ 'Message: '+req.body.message,
+		html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>' 
+    };
+
+    transporter.sendMail(mailOptions,function(error,info){
+
+        if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent: '+info.response);
+			res.redirect('/');
+		}
+    })
+
+
+});
+
+app.listen(3000);
+
+console.log('app running on http://127.0.0.1:3000');
+
+
